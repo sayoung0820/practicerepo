21648fdac155b7454c9df81f55debe3ed3c644fb
sayoung0820 <shannonashley78@gmail.com>
2022-10-28 01:17:31 -0400

    Practicing With Myself

---
+++ practicerepo.js
@@ -0,0 +1,99 @@
+// // 60 + 40
+// console.log(60 + 40)
+
+// // 50 / 50
+// console.log(50 / 50)
+
+// // 50 * 50
+// console.log(50 * 50)
+
+// 36000 * 36000touch
+// console.log(36000 * 36000)
+
+// console.log("Shannon")
+
+// console.log(45-25)
+
+// var cianiCar = "Porsche Macan"
+// var anthonyCar = "Dodge Viper"
+
+// console.log(cianiCar)
+// console.log(anthonyCar)
+
+// Exponents
+// 25 ** 5
+// console.log(25 ** 5)
+
+// Modulus/Module
+// console.log( 6 % 2)
+// console.log (5 % 2)
+// console.log("red solo cup")
+// console.log("3" , 3)
+
+// console.log("Shannon" [2])
+
+// lose equality best for comparing different data types
+// console.log(3 == 3) 
+// console.log(4 == 3)
+// console.log(2 + 1 == 3)
+// console.log("three" == 3)
+// console.log('3' == 3)
+// console.log('three' == 3)
+
+// strict equality compares the actual value
+// console.log(3 === 3)
+// console.log("three" === 3)
+// console.log('3' === 3)
+
+// greater than
+// 4 > 3
+
+// less than
+// 3 < 4
+
+// greater than or equal to
+// 5 >= 5
+// 5 >= 2
+
+// less than or equal to
+// 2 <= 5
+// 5 <= 5
+
+// console.log(5 <= 5)
+// console.log(6 >= 6)
+
+// .length is a property in strings so strings can track their own length. It looks at indexes and adds 1 to end of it
+// console.log("Shannon".length)
+// console.log("Mississippi".length)
+
+// || (logical OR) - if any come back as true, it's true
+//  && (logical and) - everything must be true or it's false
+//  ! (logical bang operator) - 
+
+// console.log(5 > 2 || 42 > 100)
+// console.log(! true)
+// console.log(! false)
+// console.log(! true && true)
+
+// Variables - placeholder for data that can be held in local memory and updated, changed, manipulated by logic
+    // examples - var, let, const
+// Can be whatever you want to name them
+// Use the assigment operator = for variables
+    // var myName = "Ashley"
+    // var ourCohort = "Golf"
+    // var myLove = "KT"
+    // var child1 = "Zoe"
+    // var child2 = "Gia"
+
+    // console.log(myName)
+    // console.log(ourCohort)
+    // // console.log(myLove)
+    // console.log(`${myName} is the best coder in ${ourCohort}`)
+    // console.log(`${myName} is going to be in a totally different tax bracket and place in life in 2023}`)
+
+    // console.log(`${child1} and ${child2} are the best things in my life!`)
+
+    
+
+
+
