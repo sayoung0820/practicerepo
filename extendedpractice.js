3fd6bf112ea34ceaad5325723d8c5c41ee63616c
sayoung0820 <shannonashley78@gmail.com>
2022-12-01 20:11:51 -0500

    completed challenge 1 solo

---
+++ extendedpractice.js
@@ -0,0 +1,70 @@
+// Write the code that will log the outcome of 34 added to 71.
+// Write the code that will log the outcome of 67 subtracted from 123.
+// Write the code that will log the outcome of 56 multiplied by 23.
+// Write the code that will log the outcome of 45 divided by 5.
+// Write the code that will log the outcome of 5 to the power of 7.
+// Write the code that will log the whole number remainder of 33 divided by 6.
+// Write the code that will log the length of a string containing your name.
+// Write the code that will log whether your string includes the letter "e"?
+// Write the code that will log the character at the first index of the string.
+// Write the code that will log the string in all uppercase letters.
+
+console.log(34 + 71)
+console.log(123 - 67)
+console.log(5 ** 7)
+console.log(33 % 6)
+let myName = "Shannon"
+console.log(myName.length)
+console.log(myName.includes("e"))
+console.log(myName[0])
+console.log(myName.indexOf("a"))
+console.log(myName.charAt(4))
+console.log(myName.toUpperCase())
+console.log(myName)
+
+
+// Write the code that will log true or false for the following:
+
+// Is 34 divided by 3 greater than 67 divided by 2?
+// Does 5 evaluate to the same as "5"?
+// Does 5 strictly equal "5"?
+// Does !3 strictly equal 3?
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+// Does "LEARN" contain the subset "RN"?
+// Does "LEARN" contain the subset "rn"?
+// Does "LEARN"[0] strictly equal "l"?
+// Modify the code from the previous question to return true.
+
+console.log(34/3 > 67/2)
+console.log(5 === "5")
+console.log(5 == "5")
+console.log(!3 === 3)
+console.log("LEARN".length === 5 && "Student".length === 10)
+console.log("LEARN".length === 9 || "LEARN".length === 9)
+console.log("LEARN".includes("RN"))
+console.log("LEARN".charAt(0) === "L")
+
+
+// Consider the variables:
+
+var theQuestion = "life, the universe, and everything"
+var theAnswer = 42
+
+// Write the code that will log theAnswer divided by 2.
+// Write the code that will log the length of theQuestion.
+// Write the code that will log the index of the character "f" in the theQuestion.
+// Write the code that will log the concatenation of the two variables.
+// Write the code that will log "the universe".
+// Write the code that will log the character "l" from theQuestion.
+// Write the code that will log whether theQuestion.length is greater than theAnswer.
+
+
+console.log(theAnswer/2)
+console.log(theQuestion.length)
+console.log(theQuestion.indexOf('f'))
+console.log("life, the universe, and everything" + " " + "42")
+console.log(theQuestion.concat(" ", theAnswer))
+console.log(theQuestion.slice(5, 18))
+console.log(theQuestion.charAt(0))
+console.log(theQuestion.length > theAnswer.length)
